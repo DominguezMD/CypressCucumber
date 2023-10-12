d1a35a69064a0a044c6459f706f4ec0a8dc4d850
DominguezMD <134455877+DominguezMD@users.noreply.github.com>
2023-10-11 21:31:34 -0300

    Delete Venex.js

--- Venex.js
+++
@@ -1,8 +0,0 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-const venex = require("../../pages/venexpage")
-
-
-Given(`un usuario este en la pagina de {string}`, (url) => {
-    cy.visit("https://www.venex.com.ar/");
-    venex.getmenubtn().should("be.visible");
-})
